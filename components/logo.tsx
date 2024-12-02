0edd59037967521e87ca980807c326742c26aaf5
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2024-12-02 11:52:03 -0500

    client side logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Logo = () => {
+  const pathname = usePathname();
+
   return (
     <Link href="/">
       <h2 className="capitalized text-2xl font-bold uppercase font-museo">
-        Venture Vox
+        {pathname === "/" && "Venture Vox"}
+        {pathname === "/startup" && "Start Up Name"}
+        {pathname === "/mybets" && "My Bets"}
       </h2>
     </Link>
   );
