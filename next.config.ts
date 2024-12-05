86199aa1434f7b51d87d5371e16e77fd22c2fb91
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2024-12-05 10:48:52 -0500

    resolve alias

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
+import path from "path";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
 
 export default nextConfig;
